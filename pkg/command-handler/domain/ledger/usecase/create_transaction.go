39e529a06ddf2144e63bf491a873a1f75a700f0b
Alex S. Garzão <alexgarzao@gmail.com>
2020-09-28 17:17:38 -0300

    fix: pointer to change global version

--- pkg/command-handler/domain/ledger/usecase/create_transaction.go
+++ pkg/command-handler/domain/ledger/usecase/create_transaction.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stone-co/the-amazing-ledger/pkg/command-handler/domain/ledger/entities"
 )
 
-func (l LedgerUseCase) CreateTransaction(ctx context.Context, id uuid.UUID, entries []entities.Entry) error {
+func (l *LedgerUseCase) CreateTransaction(ctx context.Context, id uuid.UUID, entries []entities.Entry) error {
 	transaction, err := entities.NewTransaction(id, entries...)
 	if err != nil {
 		return err
