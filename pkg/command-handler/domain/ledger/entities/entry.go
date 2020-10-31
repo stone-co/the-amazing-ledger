ac80a8868d277a6b37079a1556544c33758294fa
Alex S. Garzão <alexgarzao@gmail.com>
2020-10-31 17:35:21 -0300

    feat: entry entity has behaviour now

--- pkg/command-handler/domain/ledger/entities/entry.go
+++ pkg/command-handler/domain/ledger/entities/entry.go
@@ -7,17 +7,35 @@ import (
 type Entry struct {
 	ID        uuid.UUID
 	Operation OperationType
-	AccountID string
+	Account   *AccountName
 	Version   Version
 	Amount    int
 }
 
-func NewEntry(id uuid.UUID, operation OperationType, accountID string, version Version, amount int) *Entry {
+func NewEntry(id uuid.UUID, operation OperationType, accountID string, version Version, amount int) (*Entry, error) {
+
+	if id == uuid.Nil {
+		return nil, ErrInvalidData
+	}
+
+	if operation != CreditOperation && operation != DebitOperation {
+		return nil, ErrInvalidData
+	}
+
+	if amount <= 0 {
+		return nil, ErrInvalidData
+	}
+
+	acc, err := NewAccountName(accountID)
+	if err != nil {
+		return nil, err
+	}
+
 	return &Entry{
 		ID:        id,
 		Operation: operation,
-		AccountID: accountID,
+		Account:   acc,
 		Version:   version,
 		Amount:    amount,
-	}
+	}, nil
 }
