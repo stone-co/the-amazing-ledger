6223e9a3b7d3c2fbc0d07e77ac26650150c6600b
Jucelio Floresta <jucelio.floresta@gmail.com>
2020-11-13 17:24:30 -0300

    feat: add postgres index to class name

--- pkg/gateways/db/postgres/migrations/000001_initialize_schema.down.sql
+++ pkg/gateways/db/postgres/migrations/000001_initialize_schema.down.sql
@@ -5,4 +5,9 @@ DROP TYPE  operation_type;
 DROP TABLE accounts;
 DROP TYPE  account_type;
 
+DROP INDEX idx_entries_account_class;
+DROP INDEX idx_entries_account_group;
+DROP INDEX idx_entries_account_subgroup;
+DROP INDEX idx_entries_account_id;
+
 COMMIT;
