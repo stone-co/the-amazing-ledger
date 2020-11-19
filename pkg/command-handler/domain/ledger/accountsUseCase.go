3fd93d042d8c315db1cd2d5ffc5f4f90694cc9e6
Jucelio Floresta <jucelio.floresta@gmail.com>
2020-11-19 18:49:18 -0300

    refactor: remove account struct

--- pkg/command-handler/domain/ledger/accountsUseCase.go
+++ pkg/command-handler/domain/ledger/accountsUseCase.go
@@ -2,21 +2,3 @@ package ledger
 
 type AccountsUseCase interface {
 }
-
-type AccountInput struct {
-	Type     string   `json:"type"`
-	OwnerID  string   `json:"owner_id"`
-	Owner    string   `json:"owner"`
-	Name     string   `json:"name"`
-	Metadata []string `json:"metadata"`
-}
-
-type Account struct {
-	ID       string   `json:"id"`
-	Type     string   `json:"type"`
-	OwnerID  string   `json:"owner_id"`
-	Owner    string   `json:"owner"`
-	Name     string   `json:"name"`
-	Metadata []string `json:"metadata"`
-	Balance  int      `json:"balance"`
-}
