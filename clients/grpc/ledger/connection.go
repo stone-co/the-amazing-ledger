76fd86a49ddc268e42f289eb485ecbabae688238
Jucelio Floresta <jucelio@localhost.localdomain>
2020-12-16 19:33:34 -0300

    fix: define timeout outside connect in the sdk

--- clients/grpc/ledger/connection.go
+++ clients/grpc/ledger/connection.go
@@ -3,7 +3,6 @@ package ledger
 import (
 	"context"
 	"fmt"
-	"time"
 
 	proto "github.com/stone-co/the-amazing-ledger/gen/ledger"
 
@@ -16,10 +15,6 @@ type Connection struct {
 }
 
 func Connect(ctx context.Context, host string, port int) (*Connection, error) {
-
-	ctx, cancel := context.WithTimeout(ctx, time.Second*5)
-	defer cancel()
-
 	target := fmt.Sprintf("%s:%d", host, port)
 
 	conn, err := grpc.DialContext(ctx, target, grpc.WithInsecure(), grpc.WithBlock())
