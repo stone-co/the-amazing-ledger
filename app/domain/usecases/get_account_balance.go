ce3f71d981ccf52e60dc90fe7177c63665bc5259
Victor Lima <victoraugustolls@users.noreply.github.com>
2021-03-22 12:35:26 -0300

    refactor: better use case distinction for suffix wildcard

--- app/domain/usecases/get_account_balance.go
+++ app/domain/usecases/get_account_balance.go
@@ -3,18 +3,29 @@ package usecases
 import (
 	"context"
 
-	"github.com/jackc/pgx/v4"
-	"github.com/stone-co/the-amazing-ledger/app"
 	"github.com/stone-co/the-amazing-ledger/app/domain/vos"
 )
 
 func (l *LedgerUseCase) GetAccountBalance(ctx context.Context, accountName vos.AccountName) (*vos.AccountBalance, error) {
+	if accountName.Suffix != "*" {
+		return l.getAccountBalance(ctx, accountName)
+	}
 
+	return l.getAccountBalanceAggregated(ctx, accountName)
+}
+
+func (l *LedgerUseCase) getAccountBalance(ctx context.Context, accountName vos.AccountName) (*vos.AccountBalance, error) {
 	accountBalance, err := l.repository.GetAccountBalance(ctx, accountName)
 	if err != nil {
-		if err == pgx.ErrNoRows {
-			return nil, app.ErrAccountNotFound
-		}
+		return nil, err
+	}
+
+	return accountBalance, nil
+}
+
+func (l *LedgerUseCase) getAccountBalanceAggregated(ctx context.Context, accountName vos.AccountName) (*vos.AccountBalance, error) {
+	accountBalance, err := l.repository.GetAccountBalanceAggregated(ctx, accountName)
+	if err != nil {
 		return nil, err
 	}
 
