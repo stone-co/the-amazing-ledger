2504225a4fd09cf0b63f1032e900c04e9d577b7b
Victor Lima <victoraugustolls@users.noreply.github.com>
2021-05-21 19:19:02 -0300

    feat: updated for go.1.6

--- app/gateways/db/postgres/migrations.go
+++ app/gateways/db/postgres/migrations.go
@@ -1,24 +1,25 @@
 package postgres
 
-//go:generate go-bindata -prefix migrations/ -o migrations.gen.go -pkg postgres migrations
-
 import (
+	"embed"
+	"net/http"
+
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
-	bindata "github.com/golang-migrate/migrate/v4/source/go_bindata"
+	"github.com/golang-migrate/migrate/v4/source/httpfs"
 )
 
-func GetMigrationHandler(dbUrl string) (*migrate.Migrate, error) {
-	s := bindata.Resource(AssetNames(),
-		func(name string) ([]byte, error) {
-			return Asset(name)
-		})
+//go:embed migrations
+var migrations embed.FS
 
-	d, err := bindata.WithInstance(s)
+func GetMigrationHandler(dbUrl string) (*migrate.Migrate, error) {
+	// use httpFS until go-migrate implements ioFS (see https://github.com/golang-migrate/migrate/issues/480#issuecomment-731518493)
+	source, err := httpfs.New(http.FS(migrations), "migrations")
 	if err != nil {
 		return nil, err
 	}
-	return migrate.NewWithSourceInstance("go-bindata", d, dbUrl)
+
+	return migrate.NewWithSourceInstance("httpfs", source, dbUrl)
 }
 
 func RunMigrations(dbUrl string) error {
@@ -26,6 +27,7 @@ func RunMigrations(dbUrl string) error {
 	if err != nil {
 		return err
 	}
+
 	defer m.Close()
 	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
 		return err
