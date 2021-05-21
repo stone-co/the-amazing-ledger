12073e4a28e5c5c31ecd0798c38a61f76682d45a
Victor Lima <victoraugustolls@users.noreply.github.com>
2021-05-21 19:18:27 -0300

    refactor: refactored get analytical data query for new schema

--- app/gateways/db/postgres/get_analytical_data.go
+++ app/gateways/db/postgres/get_analytical_data.go
@@ -2,79 +2,54 @@ package postgres
 
 import (
 	"context"
+	"time"
 
 	"github.com/stone-co/the-amazing-ledger/app/domain/vos"
 	"github.com/stone-co/the-amazing-ledger/app/shared/instrumentation/newrelic"
 )
 
+const analyticalDataQuery = `
+select
+	account,
+    operation,
+    amount,
+    created_at
+from
+	entry
+where
+	account ~ $1
+order by
+	version;
+`
+
 func (r *LedgerRepository) GetAnalyticalData(ctx context.Context, path vos.AccountPath, fn func(vos.Statement) error) error {
-	operation := "Repository.GetAnalyticalData"
-	query := `
-	SELECT
-		account_class,
-		account_group,
-		account_subgroup,
-		account_id,
-		account_suffix,
-		operation,
-		amount
-	FROM
-		entries
-	`
+	const operation = "Repository.GetAnalyticalData"
 
-	args := []interface{}{}
+	defer newrelic.NewDatastoreSegment(ctx, collection, operation, analyticalDataQuery).End()
 
-	if path.TotalLevels >= 1 {
-		query += " WHERE account_class = $1"
-		args = append(args, path.Class.String())
-
-		if path.TotalLevels >= 2 {
-			query += " AND account_group = $2"
-			args = append(args, path.Group)
-
-			if path.TotalLevels >= 3 {
-				query += " AND account_subgroup = $3"
-				args = append(args, path.Subgroup)
-			}
-		}
-	}
-
-	query += " ORDER BY version"
-
-	defer newrelic.NewDatastoreSegment(ctx, collection, operation, query).End()
-
-	rows, err := r.db.Query(ctx, query, args...)
+	rows, err := r.db.Query(ctx, analyticalDataQuery, path.Name())
 	if err != nil {
 		return err
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		var class string
-		var group string
-		var subgroup string
-		var id string
-		var suffix string
-		var op string
+		var account string
+		var operation string
 		var amount int
+		var createdAt time.Time
 
 		if err = rows.Scan(
-			&class,
-			&group,
-			&subgroup,
-			&id,
-			&suffix,
-			&op,
+			&account,
 			&amount,
+			&createdAt,
 		); err != nil {
 			return err
 		}
 
-		account := vos.FormatAccount(class, group, subgroup, id, suffix)
-
 		err = fn(vos.Statement{
 			Account:   account,
-			Operation: vos.OperationTypeFromString(op),
+			Operation: vos.OperationTypeFromString(operation),
 			Amount:    amount,
 		})
 		if err != nil {
