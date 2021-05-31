815c88132d5413d5f1dc2f3e363f2d0d2478a49e
Victor Lima <victoraugustolls@users.noreply.github.com>
2021-05-30 22:02:02 -0300

    chore: removed last version from use case as it's no longer needed

--- app/domain/usecases/ledger.go
+++ app/domain/usecases/ledger.go
@@ -4,21 +4,18 @@ import (
 	"github.com/sirupsen/logrus"
 
 	"github.com/stone-co/the-amazing-ledger/app/domain"
-	"github.com/stone-co/the-amazing-ledger/app/domain/vos"
 )
 
 var _ domain.UseCase = &LedgerUseCase{}
 
 type LedgerUseCase struct {
-	log         *logrus.Logger
-	repository  domain.Repository
-	lastVersion vos.Version
+	log        *logrus.Logger
+	repository domain.Repository
 }
 
 func NewLedgerUseCase(log *logrus.Logger, repository domain.Repository) *LedgerUseCase {
 	return &LedgerUseCase{
-		log:         log,
-		repository:  repository,
-		lastVersion: 1,
+		log:        log,
+		repository: repository,
 	}
 }
