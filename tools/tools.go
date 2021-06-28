341be2c5d053a239ac64d7fcb9ed9b92ad6b1e7b
Victor Lima <victoraugustolls@users.noreply.github.com>
2021-06-28 18:30:51 -0300

    chore: updated grpc dependencies (buf, protobuf, grpc-gateway)

--- tools/tools.go
+++ tools/tools.go
@@ -3,15 +3,13 @@
 package tools
 
 import (
-	_ "github.com/bufbuild/buf/cmd/buf"                                   // buf.build
-	_ "github.com/bufbuild/buf/cmd/protoc-gen-buf-check-breaking"         // gRPC breaking changes checker
-	_ "github.com/bufbuild/buf/cmd/protoc-gen-buf-check-lint"             // gRPC linter
-	_ "github.com/golang/protobuf/protoc-gen-go"                          // protoc Go plugin
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"               // general linter
-	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway" // gRPC gateway
-	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"    // Generates swagger from gRPC definitions
-	_ "github.com/kyoh86/richgo"                                          // enrich go test outputs with text decorations
-	_ "github.com/resotto/gochk/cmd/gochk"                                // gochk architecture linter
-	_ "golang.org/x/tools/cmd/goimports"                                  //Updates imports and formats code
-	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"                     // gRPC generator
+	_ "github.com/kyoh86/richgo"           // enrich go test outputs with text decorations
+	_ "github.com/resotto/gochk/cmd/gochk" // gochk architecture linter
+	_ "golang.org/x/tools/cmd/goimports"   // Updates imports and formats code
+
+	// GRPC Gateway - https://github.com/grpc-ecosystem/grpc-gateway#usage
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
