215f3c69a567d8d8c2567cebcc5d965f9d729eff
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-07-19 15:09:59 -0300

    refact: move servers instance to rpc pkg

---
+++ app/gateways/http/handlers.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/sirupsen/logrus"
+)
+
+func VersionHandler(commit, time string) runtime.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request, pathParams map[string]string) {
+		b, err := json.Marshal(struct {
+			GitCommitHash string `json:"git_hash"`
+			BuildTime     string `json:"time"`
+		}{
+			GitCommitHash: commit,
+			BuildTime:     time,
+		})
+		if err != nil {
+			logrus.Errorf("failed to marshal version: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		_, err = w.Write(b)
+		if err != nil {
+			logrus.Errorf("failed to write version body: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}
+}
--- app/gateways/rpc/api.go
+++ app/gateways/rpc/api.go
@@ -1,16 +1,7 @@
 package rpc
 
 import (
-	"log"
-
-	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
-	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
-	"github.com/newrelic/go-agent/v3/integrations/nrgrpc"
-	"github.com/newrelic/go-agent/v3/newrelic"
 	"github.com/sirupsen/logrus"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 
 	"github.com/stone-co/the-amazing-ledger/app/domain"
 	proto "github.com/stone-co/the-amazing-ledger/gen/ledger"
@@ -28,32 +19,4 @@ func NewAPI(log *logrus.Logger, useCase domain.UseCase) *API {
 		log:     log,
 		UseCase: useCase,
 	}
-}
-
-func (a *API) NewServer(nr *newrelic.Application) *grpc.Server {
-	// Define a func to handle panic
-	dealPanic := func(p interface{}) (err error) {
-		log.Printf("panic triggered: %v", p)
-		return status.Errorf(codes.Unknown, "panic triggered: %v", p)
-	}
-
-	opts := []grpc_recovery.Option{
-		grpc_recovery.WithRecoveryHandler(dealPanic),
-	}
-
-	srv := grpc.NewServer(
-		grpc_middleware.WithUnaryServerChain(
-			grpc_recovery.UnaryServerInterceptor(opts...),
-			nrgrpc.UnaryServerInterceptor(nr),
-		),
-		grpc_middleware.WithStreamServerChain(
-			grpc_recovery.StreamServerInterceptor(opts...),
-			nrgrpc.StreamServerInterceptor(nr),
-		),
-	)
-
-	proto.RegisterLedgerServiceServer(srv, a)
-	proto.RegisterHealthServer(srv, a)
-
-	return srv
 }
