da49090110fa56f109642503791b1f3a62367432
Alexandre <alexandre@feiler.co>
2021-07-22 11:41:01 -0300

    refact: using a interface to abstract the probe monitor data function return

--- app/domain/probes/synthetic_report.go
+++ app/domain/probes/synthetic_report.go
@@ -10,10 +10,10 @@ import (
 
 const tag = "SyntheticReport[usecase]"
 
-func (i *LedgerProbe) GettingSyntheticReport(ctx context.Context, account vos.AccountQuery, startTime time.Time, endTime time.Time) {
-	i.Log(ctx, fmt.Sprintf("%s:%s/%s-%s", tag, account.Value(), startTime.String(), endTime.String()))
+func (lp *LedgerProbe) GettingSyntheticReport(ctx context.Context, account vos.AccountQuery, startTime time.Time, endTime time.Time) {
+	lp.Log(ctx, fmt.Sprintf("%s:%s/%s-%s", tag, account.Value(), startTime.String(), endTime.String()))
 }
 
-func (i *LedgerProbe) GotSyntheticReport(ctx context.Context, value string, report vos.SyntheticReport) {
-	i.Log(ctx, fmt.Sprintf("%s:%s, tc = %d, td = %d", tag, value, report.TotalCredit, report.TotalDebit))
+func (lp *LedgerProbe) GotSyntheticReport(ctx context.Context, value string, report vos.SyntheticReport) {
+	lp.Log(ctx, fmt.Sprintf("%s:%s, tc = %d, td = %d", tag, value, report.TotalCredit, report.TotalDebit))
 }
