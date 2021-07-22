585efd3891e4998668a480adec2a24e0280f85b4
Alexandre <alexandre@feiler.co>
2021-07-22 11:59:49 -0300

    fix: removed unused comments

--- app/domain/probe.go
+++ app/domain/probe.go
@@ -6,8 +6,6 @@ import (
 
 type Probe interface {
 	Log(ctx context.Context, value string)
-	// Measure(ctx context.Context, value string)
-	// Track(ctx context.Context, value string)
 	MonitorDataSegment(ctx context.Context, collection, operation, query string) Segment
 }
 
